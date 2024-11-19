4d954de24553dd4309f971bc72e47120f00a38e5
RyanFin <ryanfinlayson125@hotmail.com>
2024-11-19 00:23:53 +0000

    declare PropTypes

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,18 +1,37 @@
+import PropTypes from "prop-types";
+
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  // const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
       </div>
     </li>
   );
+}
+
+// Define PropTypes for the component
+MenuItem.propTypes = {
+  pizza: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    unitPrice: PropTypes.number.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    soldOut: PropTypes.bool.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+// Define or import the formatCurrency function
+function formatCurrency(value) {
+  return `$${value.toFixed(2)}`;
 }
 
 export default MenuItem;
